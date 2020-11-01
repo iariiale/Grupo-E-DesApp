dff85946a75cbd9df50f8744b66e286cdc689185
GonzaloGuasch <gonzaloguasch@outlook.com>
2020-11-01 09:33:29 -0300

    user profile con donation records

---
+++ frontend/src/componenets/DonationRecordCard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../styles/DonationRecordCard.css';
+
+export default function DonationRecordCard(props) {
+
+    return(
+        <div className={"card-container"}>
+            <div className={"project-name-donationDate-container"}>
+                <div className={"project-card-name"}>{props.info.projectName + " "}</div>
+                <div className={"project-card-date"}>por una cantidad de {props.info.donatedAmount} pesos</div>
+            </div>
+            <div className={"align-date"}>{props.info.donationDate + " "}</div>
+            
+        </div>
+    )
+}
--- frontend/src/componenets/userProfile.js
+++ frontend/src/componenets/userProfile.js
@@ -2,9 +2,11 @@ import React, {Fragment, useEffect, useState} from 'react';
 import NavBar from './NavBar';
 import '../styles/userProfile.css';
 import axios from 'axios';
+import DonationRecordCard from './DonationRecordCard';
 
 export default function UserProfile(props) {
     const [amountOfPoints, setAmount] = useState(0)
+    const [records, setRecords] = useState([])
     const [user, setuser] = useState({})
 
     useEffect(() => {
@@ -14,8 +16,11 @@ export default function UserProfile(props) {
         axios.get('http://localhost:8080/user/amountOfPoints/' + userJSON.userName)
             .then(res => setAmount(res.data))
             .catch(e => console.log(e))
-    
+        axios.get('http://localhost:8080/user/GetDonationsRecords/' + userJSON.userName)
+            .then(res => setRecords(res.data))
+            .catch(e => console.log(e))
     }, [])
+    let donationRecords = records.map((aRecord, i) => <DonationRecordCard info={aRecord} key={i} />)
 
     return(
         <Fragment>
@@ -23,6 +28,8 @@ export default function UserProfile(props) {
             <div className="userProfile-container">
                 <h2>{user.userName}</h2>
                 <div>Cantidad de puntos: {amountOfPoints}</div>
+                <div>Donation registry</div>
+                {donationRecords}
                 <button className={"home-search-button"}
                         onClick={() => props.history.goBack()}>Home</button>
             </div>
